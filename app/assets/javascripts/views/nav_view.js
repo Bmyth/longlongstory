6e6dec032081df01b3a6ea03b4c423f88a81ea6a
baimingming <mmbai@thouthtworks.com>
2013-06-26 15:12:02 +0800

    interval click nav.

--- app/assets/javascripts/views/nav_view.js
+++ app/assets/javascripts/views/nav_view.js
@@ -13,9 +13,20 @@ Lls.Views.Nav = Backbone.View.extend({
     },
 
     bind_navigation : function(){
-        $(".nav-btn.up").click(global_blocks_view.moveUp);
-        $(".nav-btn.down").click(global_blocks_view.moveDown);
-        $(".nav-btn.left").click(global_blocks_view.moveLeft);
-        $(".nav-btn.right").click(global_blocks_view.moveRight);
+        this.bind_mouse_nav($(".nav-btn.up"),global_blocks_view.moveUp);
+        this.bind_mouse_nav($(".nav-btn.down"),global_blocks_view.moveDown);
+        this.bind_mouse_nav($(".nav-btn.left"),global_blocks_view.moveLeft);
+        this.bind_mouse_nav($(".nav-btn.right"),global_blocks_view.moveRight);
+    },
+
+    bind_mouse_nav : function(element, event){
+        $(element).mousedown(function(){
+            event();
+            var intervalTime = 120;
+            var intervalId = window.setInterval(event, intervalTime);
+            $(this).mouseup(function(){
+                window.clearInterval(intervalId);
+            })
+        });
     }
 });
